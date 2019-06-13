524ef62167b35b084cf12cf8d96b9b7c8fa18cae
matthewFisher <m.r.f.48215@gmail.com>
2019-06-14 02:44:13 +0900

    redux working

--- reduxWrapper.js
+++ reduxWrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from "../store/configureStore"
+import store from "./src/store/configureStore"
 
 export default ({ element }) => (
   <Provider store={store()}>{element}</Provider>
